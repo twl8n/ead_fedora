95dd68c92610d997d401cddcf9ff43bd9638788c
Tom <tom@laudeman.com>
2011-08-16 15:42:35 -0400

    checkin

--- ead_fc.rb
+++ ead_fc.rb
@@ -17,6 +17,7 @@
 
 require 'rubygems'
 require 'rest-client'
+require 'nokogiri'
 
 module Ead_fc
 
@@ -39,17 +40,101 @@ class Fx_maker
     @base_url = "http://fedoraAdmin:fedoraAdmin@localhost:8983/fedora"
     @pid_namespace = "eadfc"
     @pid = gen_pid()
+    @ef_create_date = todays_date()
+    
+  end
 
+  def todays_date
+    # Example: 2011-07-18T12:34:56.789Z
+
+    # Use GMT time, but hard code the Z since Ruby %Z says "GMT" and
+    # Fedora may be expecting a Z (military time zone for UTC).
+
+    # Ruby 1.8.7 strftime() doesn't grok %L, so hard code that as '.000'.
+
+    return Time.now.utc.strftime("%Y-%m-%dT%T.000Z")
   end
 
   def read_and_parse
-    # collection data
-    collection_h = {}
-    # container list of hash. Same hash elements for each container
-    cloh = []
-    exml = IO.read(@fname)
-    
-    return [collection_h, cloh]
+    xml = Nokogiri::XML(open(@fname))
+
+    # Not used currently, but could be later.
+    ead_schema_ns = 'urn:isbn:1-931666-22-9'
+
+    # Agnostic variables. This is oop so it is ok to use instance vars
+    # because they won't cause any of the bugs that arise from using
+    # globals in imperative code. Obi wan says: These aren't the
+    # globals you're looking for.
+
+    @agn_title = xml.xpath("//*/xmlns:titleproper[@type='formal']")[0].content
+
+    # <!-- <origination label="Creator:"><persname rules="aacr" source="ingest"> -->
+
+    @agn_creator = xml.xpath("//*/xmlns:origination[@label='Creator:']/xmlns:persname")[0].content
+
+    # <!-- unitid, Can identifiers have a space? -->
+    @agn_id = xml.xpath("//*/xmlns:archdesc/xmlns:did/xmlns:unitid")[0].content
+
+    # <archdesc><scopecontent><p> Ignore <head>. Get all <p> and separate by \n -->
+
+    # Tween always at the beginning. Start with "" and change to the
+    # tween string after the first iteration. No knowing what this
+    # text will be used for, separate paragraphs with a double
+    # newline. 
+
+    tween = ""
+    @agn_scope = ""
+    xml.xpath("//*/xmlns:archdesc/xmlns:scopecontent/xmlns:p").each { |ele|
+      @agn_scope += "#{tween}#{ele.content}"
+      tween = "\n\n"
+    }
+
+    # <!-- <publisher>Yale University Library</publisher>
+    #            <publisher>Manuscripts and Archives</publisher> -->
+
+    # It is hard to know how other institutions will handle this. In
+    # the short term using ", " as the tween looks pretty reasonable.
+
+    tween = ""
+    @agn_corp_name = ""
+    xml.xpath("//*/xmlns:publicationstmt/xmlns:publisher").each { |ele|
+      @agn_corp_name += "#{tween}#{ele.content}"
+      tween = ", "
+    }
+
+    # <!-- <unitdate> -->
+
+    @agn_create_date = xml.xpath("//*/xmlns:archdesc/xmlns:did/xmlns:unitdate")[0].content
+
+    # <!-- <physdesc> -->
+
+    @agn_extent = xml.xpath("//*/xmlns:titleproper[@type='formal']")[0].content
+
+    # 	  <!-- scopecontent -->
+
+    @agn_abstract = xml.xpath("//*/xmlns:titleproper[@type='formal']")[0].content
+
+    # 	  <!-- <bioghist> (not including <head>) -->
+
+    @agn_bio = xml.xpath("//*/xmlns:titleproper[@type='formal']")[0].content # bioghist
+
+    # <!-- <acqinfo> -->
+
+    @agn_acq_info = xml.xpath("//*/xmlns:titleproper[@type='formal']")[0].content
+
+    # <!-- prefercite -->
+
+    @agn_cite = xml.xpath("//*/xmlns:titleproper[@type='formal']")[0].content
+
+    # ?
+    @agn_type = xml.xpath("//*/xmlns:titleproper[@type='formal']")[0].content
+
+    @agn_object_type = xml.xpath("//*/xmlns:titleproper[@type='formal']")[0].content
+    @agn_set_type = xml.xpath("//*/xmlns:titleproper[@type='formal']")[0].content
+
+    @agn_agreement_id = xml.xpath("//*/xmlns:titleproper[@type='formal']")[0].content
+    @agn_project = xml.xpath("//*/xmlns:titleproper[@type='formal']")[0].content
+
   end
 
 

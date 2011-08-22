fbcc590566fc563e9fa4f0c84f58d71303fd7617
Tom <tom@laudeman.com>
2011-08-22 16:28:39 -0400

    container algo working, getting element data

--- ead_fc.rb
+++ ead_fc.rb
@@ -73,36 +73,167 @@ class Fx_maker
 
     collection_parse(rh)
 
-    push(@cn_loh, rh)
-
-    # Process the container elements. Hashes are pass byreference,
-    # but Fixnums (ints) are pass by value.
-
-    con_count = 1
-    while (con_count = container_parse(rh, con_count))
-      # rh['is_container'] = true
-      push(@cn_loh, rh)
-    end
-
-    @cn_loh.each { |@cn|
-
-      # Render (and load) the template for each hash in the list of hashes.
-      
-      # Since we are using instance vars which are essentially global,
-      # we might not need binding() which passes the current execution
-      # heap space.
-      
-      @xml_out = generic_template.result(binding())
-      
-      # load into Fedora
-    }
-
-  end
-
-  def container_parse(rh, con_count)
+    # Since we are using instance vars which are essentially global,
+    # we might not need binding() which passes the current execution
+    # heap space.
     
+    @xml_out = generic_template.result(binding())
+    ingest_internal()
+    write_foxml(rh['pid'])
+    @cn_loh.push(rh)
 
-    return con_count
+    # Process the container elements, parse, create foxml, ingest, write to file.
+
+    nset = @xml.xpath("//*/xmlns:archdesc/xmlns:dsc")
+    container_parse(nset)
+
+  end # initialize
+
+  def container_parse(nset)
+
+    # Modify @cn_loh.
+    
+    rh = Hash.new()
+
+    # If we aren't using the index xx, remove it.
+
+    nset.children.each_with_index { |ele,xx|
+      if ele.name.match(/^c\d+/)
+
+        # Actions to implment here: Get Fedora PID. Get parent PID
+        # from the stack. Save current node info in a hash, push onto
+        # the container stack, generate foxml, ingest.
+
+        rh['pid'] = gen_pid()
+        rh['ef_create_date'] = @ef_create_date
+        rh['is_container'] = true
+        rh['type_of_resource'] = 'container="yes"'
+        rh['parent_pid'] =  @cn_loh.last['pid']
+
+        # container id (attr), container level (attr),
+        # container (element, c01, c02, ...), unittitle, container
+        # type (attr), container (value, string, could be "6-7"), may be
+        # multiple <container> elements), unitdate, scopecontent
+
+        # container_element
+        rh['container_element'] = ele.name
+        rh['container_level'] = ele.attribute('level')
+        rh['container_id'] = ele.attribute('id')
+
+        # Note: container_type and container_value need to be a list
+        # of hash due to possible multiple values!
+        
+        # These seem to work too, returning the expected single value
+        # or nil when there isn't a unitdate.
+
+        # nset.xpath("./xmlns:c02/xmlns:c03/xmlns:did").children.xpath("./xmlns:unitdate")[0]
+
+        # nset.xpath("./xmlns:c02/xmlns:c03/xmlns:did")[0].xpath("./xmlns:unitdate")[0]
+        
+        # Default values.
+
+        rh['container_unitdate'] = ""
+        rh['container_unittitle'] = ""
+
+        # <container> element(s) are in a list of hashes.
+        rh['ct'] = Array.new()
+        nset.xpath("./xmlns:c02/xmlns:c03/xmlns:did")[0].children.each { |child|
+          if child.name.match(/container/)
+            ch = Hash.new
+            ch['container_type'] = child.attribute('type')
+            ch['container_value'] = child.content
+            rh['ct'].push(ch)
+          end
+          
+          if child.name.match(/unitdate/)
+            rh['container_unitdate'] = child.content
+          end
+          
+          if child.name.match(/unittitle/)
+            rh['container_unittitle'] = child.content
+          end
+        }
+
+        if ele.name.match(/c01/)
+          
+          # Only occurs as a child of c01
+          container_scopecontent
+        end
+
+        # collection info
+
+        rh['titleproper'] = @xml.xpath("//*/xmlns:titleproper[@type='formal']")[0].content
+        rh['title'] = @xml.xpath("//*/xmlns:archdesc/xmlns:did/xmlns:unittitle")[0].content
+        rh['creator'] = @xml.xpath("//*/xmlns:origination[@label='Creator:']/xmlns:persname")[0].content
+        
+        rh['id'] = @xml.xpath("//*/xmlns:archdesc/xmlns:did/xmlns:unitid")[0].content
+
+        tween = ""
+        rh['scope'] = ""
+        @xml.xpath("//*/xmlns:archdesc/xmlns:scopecontent/xmlns:p").each { |ele|
+          rh['scope'] += "#{tween}#{ele.content}"
+          tween = "\n\n"
+        }
+        
+        tween = ""
+        rh['corp_name'] = ""
+        @xml.xpath("//*/xmlns:publicationstmt/xmlns:publisher").each { |ele|
+          rh['corp_name'] += "#{tween}#{ele.content}"
+          tween = ", "
+        }
+        rh['create_date'] = @xml.xpath("//*/xmlns:archdesc/xmlns:did/xmlns:unitdate")[0].content
+        
+        rh['extent'] = @xml.xpath("//*/xmlns:archdesc/xmlns:did/xmlns:physdesc/xmlns:extent")[0].content
+        
+        rh['abstract'] = @xml.xpath("//*/xmlns:archdesc/xmlns:did/xmlns:abstract")[0].content
+        tween = ""
+        rh['bio'] = ""
+        @xml.xpath("//*/xmlns:archdesc/xmlns:bioghist/xmlns:p").each { |ele|
+          rh['bio'] += "#{tween}#{ele.content}"
+          tween = "\n\n"
+        }
+        
+        tween = ""
+        rh['acq_info'] = ""
+        @xml.xpath("//*/xmlns:archdesc/xmlns:descgrp/xmlns:descgrp/xmlns:acqinfo/xmlns:p").each { |ele|
+          rh['acq_info'] += "#{tween}#{ele.content}"
+          tween = "\n\n"
+        }
+        
+        tween = ""
+        rh['cite'] = ""
+        @xml.xpath("//*/xmlns:archdesc/xmlns:descgrp/xmlns:prefercite/xmlns:p").each { |ele|
+          rh['cite'] += "#{tween}#{ele.content.strip}"
+          tween = "\n\n"
+        }
+
+        rh['type'] = @xml.xpath("//*/xmlns:archdesc")[0].attributes['level']
+        
+        if (rh['type'] == "collection")
+          rh['object_type'] = "set"
+          rh['set_type'] = rh['type']
+        end
+        
+        rh['agreement_id'] = ""
+        rh['project'] = rh['title']
+        
+        
+        @xml_out = generic_template.result(binding())
+        ingest_internal()
+        write_foxml(rh['pid'])
+
+        result = container_parse(ele)
+
+        # Actions to implment here: Pop stack. When we eventually
+        # implement "isParentOf" then this is where we will modify the
+        # Fedora object created above to know about all the children
+        # created during the recursion.
+
+        @cn_loh.pop()
+
+      end
+    }
+    return "#{nset.name} #{nset.get_attribute('id')}"
   end
 
 
@@ -120,14 +251,11 @@ class Fx_maker
     
     rh['title'] = @xml.xpath("//*/xmlns:archdesc/xmlns:did/xmlns:unittitle")[0].content
     
-    # <!-- <origination label="Creator:"><persname rules="aacr" source="ingest"> -->
-    
     rh['creator'] = @xml.xpath("//*/xmlns:origination[@label='Creator:']/xmlns:persname")[0].content
     
-    # <!-- unitid, Can identifiers have a space? -->
     rh['id'] = @xml.xpath("//*/xmlns:archdesc/xmlns:did/xmlns:unitid")[0].content
     
-    # <archdesc><scopecontent><p> Ignore <head>. Get all <p> and separate by \n -->
+    # Ignore <head>. Get all <p> and separate by \n -->
     
     # Tween always at the beginning. Start with "" and change to the
     # tween string after the first iteration. No knowing what this
@@ -141,9 +269,6 @@ class Fx_maker
       tween = "\n\n"
     }
     
-    # <!-- <publisher>Yale University Library</publisher>
-    #            <publisher>Manuscripts and Archives</publisher> -->
-
     # It is hard to know how other institutions will handle this. In
     # the short term using ", " as the tween looks pretty reasonable.
 
@@ -154,8 +279,6 @@ class Fx_maker
       tween = ", "
     }
     
-    # <!-- <unitdate> -->
-
     rh['create_date'] = @xml.xpath("//*/xmlns:archdesc/xmlns:did/xmlns:unitdate")[0].content
 
     rh['extent'] = @xml.xpath("//*/xmlns:archdesc/xmlns:did/xmlns:physdesc/xmlns:extent")[0].content
@@ -172,8 +295,6 @@ class Fx_maker
       tween = "\n\n"
     }
 
-    # <!-- <acqinfo> -->
-
     tween = ""
     rh['acq_info'] = ""
     @xml.xpath("//*/xmlns:archdesc/xmlns:descgrp/xmlns:descgrp/xmlns:acqinfo/xmlns:p").each { |ele|
@@ -181,8 +302,6 @@ class Fx_maker
       tween = "\n\n"
     }
     
-    # <!-- prefercite -->
-
     tween = ""
     rh['cite'] = ""
     @xml.xpath("//*/xmlns:archdesc/xmlns:descgrp/xmlns:prefercite/xmlns:p").each { |ele|
@@ -214,17 +333,13 @@ class Fx_maker
     rh['project'] = rh['title']
   end # collection_parse
   
-
   
-  def write_foxml
-    if true
-      fn = fxm.pid + ".xml"
-      fn.gsub!(/:/,"_")
-      File.open(fn, "wb") { |my_xml|
-        my_xml.write(fxm.xml_out())
-      }
-      print "Wrote: #{fn}\n"
-    end
+  def write_foxml(pid)
+    fn = pid + ".xml"
+    fn.gsub!(/:/,"_")
+    File.open(fn, "wb") { |my_xml|
+      my_xml.write(@xml_out)
+    }
   end
 
 

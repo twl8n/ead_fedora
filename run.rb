9e72ba3ef17071920a491d98d00a98262dadd1c3
Tom <tom@laudeman.com>
2011-09-29 15:51:23 -0400

    fix run.rb docs, comments, slight internal logic.

--- run.rb
+++ run.rb
@@ -5,16 +5,16 @@ require 'ead_fc'
 # unbuffer output.
 STDOUT.sync = true
 
-# ead xml file string, debug boolean
-
-myfile = ARGV[0]
-
-require myfile
-
 if ARGV.size == 0
   print "Usage: #{$0} collction_config_file.rb\n"
   exit
 end
+
+# The command line arg is the config file name
+
+myfile = ARGV[0]
+
+require myfile
 
 if File.exists?(Ead_file)
   fxm = Ead_fc::Fx_maker.new(Ead_file, Fx_debug)
